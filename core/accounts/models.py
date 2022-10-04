d1cef59d6946989f3b1a314ecc61394c8325afeb
DevPrinceK <princesamuelpks@gmail.com>
2022-10-04 21:33:17 +0000

    Update models.py

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -10,18 +10,26 @@ class User(AbstractBaseUser, PermissionsMixin):
     lname = models.CharField(max_length=255, blank=True, null=True)
     gender = models.CharField(max_length=1, blank=True, null=True)
     phone = models.CharField(max_length=20, blank=True, null=True)
-    birthday = models.DateField(blank=True, null=True)
+    dob = models.DateField(blank=True, null=True)
     region = models.CharField(max_length=255, blank=True, null=True)
     city = models.CharField(max_length=255, blank=True, null=True)
     zip = models.CharField(max_length=10, blank=True, null=True)
     country = models.CharField(max_length=255, blank=True, null=True)
+    role = models.CharField(max_length=255, blank=True, null=True)
+    created_from_dashboard = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+    is_appuser = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
 
     def get_fullname(self):
+        '''return the full name of the user'''
         return self.fname + ' ' + self.lname if self.fname or self.lname else 'N/A'  # noqa
+
+    def resend_user_password(self):
+        '''sends the user password to the user's email'''
+        pass
 
     objects = AccountManager()
 
@@ -32,3 +40,17 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     class Meta:
         db_table = 'user'
+
+
+class OTP(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    otp = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expiry_date = models.DateTimeField(blank=True, null=True)
+    is_expired = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.email
+
+    class Meta:
+        db_table = 'otp'
