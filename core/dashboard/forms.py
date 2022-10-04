395e15cbdac9fc5239975f54a5dedc103cf9c3de
DevPrinceK <princesamuelpks@gmail.com>
2022-10-04 21:34:27 +0000

    Create forms.py

---
+++ core/dashboard/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+from accounts.models import User
+
+
+class UserForm(forms.ModelForm):
+    '''Form for creating a new user'''
+    class Meta:
+        model = User
+        fields = ['fname', 'lname', 'email', 'gender', 'phone', 'dob', 'region', 'city', 'zip', 'country', 'role', ]  # noqa
