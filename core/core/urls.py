2f08133ebafb14a33839f719dc71b578b1bad452
DevPrinceK <princesamuelpks@gmail.com>
2022-10-05 10:00:53 +0000

    Update urls.py

--- core/core/urls.py
+++ core/core/urls.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('dashboard/', include('dashboard.urls')),
 ]
+
+# rest_framework auth
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
+]
+
+# Let django serve static files in development mode
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
