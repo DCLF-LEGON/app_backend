bd374d0dc4c9b298154228abd089b2592aab492a
DevPrinceK <princesamuelpks@gmail.com>
2022-10-05 17:50:48 +0000

    Update manager.py

--- core/accounts/manager.py
+++ core/accounts/manager.py
@@ -1,17 +1,21 @@
 from django.contrib.auth.models import BaseUserManager
+from core.utils.constants import UserRoles
 
 
 class AccountManager(BaseUserManager):
     '''manages User account creation'''
 
-    def create_user(self, email, password, **kwargs):
-        user = self.model(email=email, password=password, **kwargs)
+    def create_user(self, email, password, fname, lname, **kwargs):
+        user = self.model(email=email, password=password,
+                          fname=fname, lname=lname, **kwargs)
         user.set_password(password)
+        user.is_appuser = True
+        user.role = UserRoles.app_user.value
         user.save()
         return user
 
-    def create_superuser(self, email, password, **kwargs):
-        user = self.create_user(email,  password, **kwargs)
+    def create_superuser(self, email, password, fname, lname, **kwargs):
+        user = self.create_user(email,  password, fname, lname, **kwargs)
         user.is_superuser = True
         user.is_staff = True
         user.save()
