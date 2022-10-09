7714b96e3f365a12a89fce2b27b8df6521e1c5ed
DevPrinceK <princesamuelpks@gmail.com>
2022-10-09 22:28:41 +0000

    Update views.py

--- core/api/views.py
+++ core/api/views.py
@@ -1,3 +1,6 @@
+import random
+import string
+from dashboard.signals import generate_otp
 from django.shortcuts import render
 from accounts.models import OTP
 from api.serializers import RegisterSerializer, UserSerializer
@@ -17,6 +20,9 @@ class ApiEndPointsView(APIView):
             'sign-up': '/api/sign-up/',
             'login': '/api/login/',
             'user-profile': '/api/user-profile/',
+
+            'verify-otp': '/api/verify-otp/',
+            'resend-otp': '/api/resend-otp/',
         })
 
 
@@ -33,6 +39,7 @@ class LoginAPI(KnoxLoginView):
 
 
 class SignUpAPI(generics.GenericAPIView):
+    '''This CBV is used to register a new user'''
     permission_classes = [permissions.AllowAny]
     serializer_class = RegisterSerializer
 
@@ -47,17 +54,17 @@ class SignUpAPI(generics.GenericAPIView):
 
 
 class VerifyOTPAPI(generics.GenericAPIView):
+    '''This CBV is used to verify a user's OTP'''
     permission_classes = [permissions.AllowAny]
 
     def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
         otp_code = request.data.get('otp')
         token_key = request.META.get('HTTP_AUTHORIZATION').split(' ')[1][0:8]
         if token_key:
             token = AuthToken.objects.get(token_key=token_key)
             user = token.user
             otp = OTP.objects.filter(user=user, otp=otp_code).first()
-            if otp and otp.otp_is_expired() == False:
+            if otp and (otp.otp_is_expired() == False):
                 otp.delete()
                 return Response({
                     "message": "OTP verified successfully",
@@ -70,3 +77,31 @@ class VerifyOTPAPI(generics.GenericAPIView):
             "user": UserSerializer(user).data,
             "token": AuthToken.objects.create(user)[1],
         }, status=status.HTTP_201_CREATED)
+
+
+class ResendOTPAPI(generics.GenericAPIView):
+    '''This CBV is used to resend a user's OTP'''
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        token_key = request.META.get('HTTP_AUTHORIZATION').split(' ')[1][0:8]
+        if token_key:
+            token = AuthToken.objects.filter(token_key=token_key).first()
+            if token:
+                user = token.user
+            else:
+                return Response({
+                    "message": "Invalid token!",
+                }, status=status.HTTP_400_BAD_REQUEST)
+            otp = OTP.objects.filter(user=user).first()
+            if otp:
+                otp.delete()
+            # The create function will create a new OTP
+            # and trigger a signal that will send the OTP to the user's mail
+            otp = OTP.objects.create(user=user, otp=generate_otp())
+            return Response({
+                "message": f"OTP resent successfully to {otp.user.email}",
+            }, status=status.HTTP_200_OK)
+        return Response({
+            "message": "Invalid token!",
+        }, status=status.HTTP_400_BAD_REQUEST)
