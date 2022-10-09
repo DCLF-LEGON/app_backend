155939706265ed3dd494ace1f2ed4b5d5bf747e5
DevPrinceK <princesamuelpks@gmail.com>
2022-10-09 22:29:43 +0000

    Update users.py

--- core/dashboard/views/users.py
+++ core/dashboard/views/users.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.views import View
 from django.http import HttpResponseRedirect
+from accounts.models import User
 from dashboard.forms import UserForm
 
 
@@ -9,7 +10,10 @@ class UsersListView(View):
     template = 'dashboard/users.html'
 
     def get(self, request):
-        context = {}
+        users = User.objects.all().order_by('-id')
+        context = {
+            "users": users,
+        }
         return render(request, self.template, context)
 
 
@@ -24,6 +28,16 @@ class CreateUpdateUserView(View):
         return render(request, self.template, context)
 
     def post(self, request, *args, **kwargs):
+        user_id = request.POST.get('user_id')
+        # new user
+        if not user_id:
+            form = UserForm(request.POST, request.FILES or None)  # noqa
+            if form.is_valid():
+                form.save()
+                return HttpResponseRedirect('/dashboard/users')
+            else:
+                return HttpResponse('form is not valid')
+        # update user
         form = UserForm(request.POST)
         if form.is_valid():
             form.save()
