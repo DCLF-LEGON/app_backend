fcfd0326597679315240aeb153a8ecc6e01e15a8
DevPrinceK <princesamuelpks@gmail.com>
2022-10-12 15:01:29 +0000

    Update leaders.py

--- core/dashboard/views/leaders.py
+++ core/dashboard/views/leaders.py
@@ -10,3 +10,16 @@ class LeadersListView(View):
     def get(self, request, *args, **kwargs):
         context = {}
         return render(request, self.template, context)
+
+
+class CreateLeaderView(View):
+    '''CBV for create leader page'''
+
+    template = 'dashboard/form-renderers/create_update_leader.html'
+
+    def get(self, request, *args, **kwargs):
+
+        context = {
+            "head": "Create"
+        }
+        return render(request, self.template, context)
