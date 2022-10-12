c8b6a98bed3b4ac51da9247a869aad577e605eaf
DevPrinceK <princesamuelpks@gmail.com>
2022-10-12 15:04:48 +0000

    Update manager.py

--- core/accounts/manager.py
+++ core/accounts/manager.py
@@ -5,18 +5,18 @@ from core.utils.constants import UserRoles
 class AccountManager(BaseUserManager):
     '''manages User account creation'''
 
-    def create_user(self, email, password, fname='-', lname='-', **kwargs):
+    def create_user(self, email, password, fullname='-', **kwargs):
         user = self.model(email=email, password=password,
-                          fname=fname, lname=lname, **kwargs)
+                          fullname=fullname, **kwargs)
         user.set_password(password)
         user.is_appuser = True
         user.role = UserRoles.app_user.value
         user.save()
         return user
 
-    def create_superuser(self, email, password, fname='-', lname='-', **kwargs):
+    def create_superuser(self, email, password, fullname='-', **kwargs):
         user = self.create_user(
-            email,  password, fname='-', lname='-', **kwargs)
+            email,  password, fullname='-', **kwargs)
         user.is_superuser = True
         user.role = UserRoles.administrator.value
         user.is_staff = True
