f5de0db070d4d976ec677dba418421fa1eac823c
DevPrinceK <princesamuelpks@gmail.com>
2022-10-17 20:13:24 +0000

    Update forms.py

--- core/dashboard/forms.py
+++ core/dashboard/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 
 from accounts.models import User
+from dashboard.models import Leader
+from dashboard.models import Preacher
 
 
 class UserForm(forms.ModelForm):
@@ -8,3 +10,17 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['fullname', 'email', 'gender', 'phone', 'dob', 'region', 'city', 'zip', 'country', 'role', ]  # noqa
+
+
+class LeaderForm(forms.ModelForm):
+    '''Form for creating a new leader'''
+    class Meta:
+        model = Leader
+        fields = ['name', 'title', 'position', 'picture', ]
+
+
+class PreacherForm(forms.ModelForm):
+    '''Form for creating a new preacher'''
+    class Meta:
+        model = Preacher
+        fields = ['name', 'title', 'position', 'picture', ]
