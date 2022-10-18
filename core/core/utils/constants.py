cb4247e0b55e6c8475707690e01a408a0f7dbc5e
DevPrinceK <princesamuelpks@gmail.com>
2022-10-18 22:10:22 +0000

    Update constants.py

--- core/core/utils/constants.py
+++ core/core/utils/constants.py
@@ -5,3 +5,9 @@ class UserRoles(Enum):
     '''Defines the user roles in the system'''
     app_user = 'app_user'
     administrator = 'administrator'
+
+
+class MediaType(Enum):
+    '''Defines the media types'''
+    audio = 'audio'
+    video = 'video'
