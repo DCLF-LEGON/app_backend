f05338f384aa5acc85c93cab10a05e8c6a0a2fb5
DevPrinceK <princesamuelpks@gmail.com>
2022-10-18 22:10:29 +0000

    Update forms.py

--- core/dashboard/forms.py
+++ core/dashboard/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 from accounts.models import User
-from dashboard.models import Leader
+from dashboard.models import Doctrine, Leader, Message, MessageCategory
 from dashboard.models import Preacher
 
 
@@ -24,3 +24,24 @@ class PreacherForm(forms.ModelForm):
     class Meta:
         model = Preacher
         fields = ['name', 'title', 'position', 'picture', ]
+
+
+class DoctrineForm(forms.ModelForm):
+    '''Form for creating a new doctrine'''
+    class Meta:
+        model = Doctrine
+        fields = ['title', 'description', ]
+
+
+class MessageCategoryForm(forms.ModelForm):
+    '''Form for creating a new message category'''
+    class Meta:
+        model = MessageCategory
+        fields = ['name', ]
+
+
+class MessageForm(forms.ModelForm):
+    '''Form for creating a new message'''
+    class Meta:
+        model = Message
+        fields = ['title', 'media', 'media_type', ]  # noqa
