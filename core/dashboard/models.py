2eca08ca9a105335436f0d76723184c7824f92d6
DevPrinceK <princesamuelpks@gmail.com>
2022-10-18 22:11:01 +0000

    Update models.py

--- core/dashboard/models.py
+++ core/dashboard/models.py
@@ -1,6 +1,8 @@
+import os
 from accounts.models import User
-from tkinter import Image
 from django.db import models
+
+from core.utils.constants import MediaType
 
 
 class Leader(models.Model):
@@ -45,3 +47,39 @@ class Doctrine(models.Model):
 
     class Meta:
         db_table = 'doctrine'
+
+
+class MessageCategory(models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'category'
+
+
+class Message(models.Model):
+    title = models.CharField(max_length=200)
+    media = models.FileField(upload_to='assets/messages/', blank=True, null=True)  # noqa
+    media_type = models.CharField(max_length=10, choices=(
+        (MediaType.audio.value, MediaType.audio.value),
+        (MediaType.video.value, MediaType.video.value),
+    ), default=MediaType.video.value)
+    preacher = models.ForeignKey(Preacher, related_name='messages', on_delete=models.CASCADE)  # noqa
+    category = models.ForeignKey(MessageCategory, related_name='messages', on_delete=models.CASCADE)  # noqa
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def get_media_size(self):
+        if self.media:
+            return f'{round(os.path.getsize(self.media.path) / 1024 / 1024, 2)} MB'  # noqa
+        return '--- MB'
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'message'
