57bf20c56909d5c20903ac8a0feea7d4841a0aec
DevPrinceK <princesamuelpks@gmail.com>
2022-10-23 00:01:39 +0000

    Update donations.py

--- core/dashboard/views/donations.py
+++ core/dashboard/views/donations.py
@@ -1,5 +1,12 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.views import View
+
+from dashboard.models import Donation
+
+import datetime
+
+# from django.utils.timzone import now
 
 
 class DonationsListView(View):
@@ -8,5 +15,36 @@ class DonationsListView(View):
     template = 'dashboard/donations.html'
 
     def get(self, request, *args, **kwargs):
-        context = {}
+        donations = Donation.objects.all().order_by('-created_at')
+        # today = now().date()
+        today = datetime.date.today()
+        donations_today = Donation.objects.filter(created_at__date=today).count()  # noqa
+        context = {
+            'donations': donations,
+            'donations_today': donations_today,
+            'total_donations': donations.count(),
+        }
+        return render(request, self.template, context)
+
+
+class SearchDonationView(View):
+    '''CBV for searching donations'''
+
+    template = 'dashboard/donations.html'
+
+    def get(self, request, *args, **kwargs):
+        query = request.GET.get('q')
+        donations = Donation.objects.filter(
+            Q(transaction_id__icontains=query) |
+            Q(amount__icontains=query) |
+            Q(network__icontains=query) |
+            Q(mobile_number__icontains=query) |
+            Q(status_code__icontains=query) |
+            Q(status_message__icontains=query)
+        ).order_by('-created_at')
+        context = {
+            'donations': donations,
+            'total_donations': donations.count(),
+            'search': True,
+        }
         return render(request, self.template, context)
