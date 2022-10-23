4e708b14c8e0317db7c69c68fe1cb37155628b52
DevPrinceK <princesamuelpks@gmail.com>
2022-10-23 00:00:57 +0000

    Create util_functions.py

---
+++ core/core/utils/util_functions.py
@@ -0,0 +1,56 @@
+import requests
+from core import settings
+
+
+def receive_payment(data):
+    ENDPOINT = 'https://payhubghana.io/api/v1.0/debit_mobile_account/'
+    headers = {
+        "Authorization": f"Token {settings.PAYHUB_SECRET_TOKEN}",
+    }
+    response = requests.post(ENDPOINT, data=data, headers=headers)
+    response_data = response.json()
+    print('From receive_payment', response_data)
+    return response_data
+
+
+def make_payment(data):
+    ENDPOINT = 'https://payhubghana.io/api/v1.0/credit_mobile_account/'
+    headers = {
+        "Authorization": f"Token {settings.PAYHUB_SECRET_TOKEN}",
+    }
+    response = requests.post(ENDPOINT, data=data, headers=headers)
+    response_data = response.json()
+    print("From make_payment", response_data)
+    return response_data
+
+
+def get_transaction_status(transaction_id):
+    ENDPOINT = 'https://payhubghana.io/api/v1.0/transaction_status'
+    headers = {
+        "Authorization": f"Token {settings.PAYHUB_SECRET_TOKEN}",
+    }
+    params = {
+        "transaction_id": transaction_id,
+    }
+    response = requests.get(ENDPOINT, params=params, headers=headers)
+    print('Raw response from get_transaction_status: ', response)
+    response_data = response.json()
+    print('From get_transaction_status: ', response_data)
+    return response_data
+
+
+def get_api_wallet_balance():
+    ENDPOINT = 'https://payhubghana.io/api/v1.0/wallet_balance'
+    headers = {
+        "Authorization": f"Token {settings.PAYHUB_SECRET_TOKEN}",
+    }
+    payload = {
+        "wallet_id": settings.PAYHUB_WALLET_ID,
+    }
+    response = requests.get(ENDPOINT, headers=headers, params=payload)
+    try:
+        main_balance = response.json()['main_balance']
+        main_balance = decimal.Decimal(main_balance)
+    except KeyError:
+        main_balance = decimal.Decimal(0)
+    return main_balance
