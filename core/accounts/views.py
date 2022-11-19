fcfdae64fd698f6ee95c364f12c6ebf250dc5f4e
DevPrinceK <princesamuelpks@gmail.com>
2022-11-19 13:38:45 +0000

    Update views.py

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -13,18 +13,20 @@ class LoginView(View):
     def post(self, request):
         email = request.POST.get('email')
         password = request.POST.get('password')
-        user = authenticate(request, username=email, password=password)
-        if user is not None and user.is_staff:
-            # redirect to otp page
-            # do the redirection here
-            login(request, user)
-            return redirect('dashboard')
-        else:
-            messages.error(request, 'Invalid credentials')
-            return redirect('login')
+        # user = authenticate(request, username=email, password=password)
+        # if user is not None and user.is_staff:
+        #     # redirect to otp page
+        #     # do the redirection here
+        #     login(request, user)
+        #     return redirect('dashboard')
+        # else:
+        #     messages.error(request, 'Invalid credentials')
+        #     return redirect('accounts:login')
+        # NOTE: This is just a temporary code to test the dashboard
+        return redirect('dashboard:dashboard')
 
 
 class LogoutView(View):
     def get(self, request):
         logout(request)
-        return redirect('login')
+        return redirect('accounts:login')
