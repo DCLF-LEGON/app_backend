8e7a28273130ccc9115d09584e0633ebdcb46b44
DevPrinceK <princesamuelpks@gmail.com>
2022-12-04 14:08:57 +0000

    Update users.py

--- core/dashboard/views/users.py
+++ core/dashboard/views/users.py
@@ -8,11 +8,17 @@ from core.utils.constants import UserRoles
 from accounts.models import User
 from dashboard.forms import UserForm
 
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+from django.utils.decorators import method_decorator
+from core.utils.decorators import AdminOnly, MustLogin
+
 
 class UsersListView(View):
     '''CBV for the users page'''
     template = 'dashboard/users.html'
 
+    @method_decorator(MustLogin)
     def get(self, request):
         query = request.GET.get('q')
         if query == UserRoles.app_user.value:
@@ -34,6 +40,7 @@ class CreateUpdateUserView(View):
     '''CBV for creating and updating users'''
     template = 'dashboard/form-renderers/create_update_user.html'
 
+    @method_decorator(MustLogin)
     def get(self, request, *args, **kwargs):
         user_id = request.GET.get('user_id')
         # new user
@@ -47,6 +54,7 @@ class CreateUpdateUserView(View):
         }
         return render(request, self.template, context)
 
+    @method_decorator(MustLogin)
     def post(self, request, *args, **kwargs):
         user_id = request.POST.get('user_id') or None
         user = User.objects.filter(id=user_id).first()
@@ -80,12 +88,22 @@ class UserDetailsView(View):
     '''CBV for user details'''
     template = 'dashboard/details/details_user.html'
 
+    @method_decorator(MustLogin)
     def get(self, request, *args, **kwargs):
         user_id = request.GET.get('user_id')
         user = User.objects.filter(id=user_id).first()
-        context = {
-            'user': user,
-        }
+        groups = Group.objects.all().order_by('name')
+        if user:
+            permissions = Permission.objects.all().order_by('name')
+            saved_permissions = user.user_permissions.all()
+            saved_groups = user.groups.all()
+            context = {
+                'user': user,
+                'groups': groups,
+                'permissions': permissions,
+                'saved_permissions': saved_permissions,
+                'saved_groups': saved_groups,
+            }
         return render(request, self.template, context)
 
 
@@ -93,6 +111,7 @@ class SearchUsersView(View):
     '''CBV for searching users'''
     template = 'dashboard/users.html'
 
+    @method_decorator(MustLogin)
     def get(self, request, *args, **kwargs):
         query = request.GET.get('q')
         users = User.objects.filter(
@@ -110,6 +129,11 @@ class SearchUsersView(View):
 class DeleteUserView(View):
     '''CBV for deleting users'''
 
+    @method_decorator(MustLogin)
+    def get(self, request, *args, **kwargs):
+        return redirect('dashboard:users')
+
+    @method_decorator(MustLogin)
     def post(self, request, *args, **kwargs):
         user_id = request.POST.get('user_id')
         user = User.objects.filter(id=user_id).first()
@@ -119,3 +143,68 @@ class DeleteUserView(View):
         else:
             messages.info(request, 'Error deleting user')
         return redirect('dashboard:users')
+
+
+class AddUserToGroupsView(View):
+    '''CBV for adding user to groups'''
+    template = 'dashboard/details/details_user.html'
+
+    @method_decorator(MustLogin)
+    def get(self, request, *args, **kwargs):
+        user_id = request.GET.get('user_id') or None
+        user = User.objects.filter(id=user_id).first()
+        groups = Group.objects.all().order_by('name')
+        if user:
+            permissions = Permission.objects.all().order_by('name')
+            saved_permissions = user.user_permissions.all()
+            saved_groups = user.groups.all()
+            context = {
+                'user': user,
+                'permissions': permissions,
+                'saved_groups': saved_groups,
+                'groups': groups,
+                'saved_permissions': saved_permissions,
+            }
+            return render(request, self.template, context)  # noqa
+        messages.info(request, 'User Does Not Exist')
+        return redirect('dashboard:users')
+
+    @method_decorator(MustLogin)
+    def post(self, request, *args, **kwargs):
+        user_id = request.POST.get('user_id') or None
+        user = User.objects.filter(id=user_id).first()
+        user_groups = request.POST.getlist('groups')
+        if user:
+            user.groups.clear()
+            for group in user_groups:
+                item = Group.objects.filter(id=group).first()
+                user.groups.add(item)
+            user.save()
+            messages.success(request, 'User Groups Updated Successfully')  # noqa
+        else:
+            messages.info(request, 'User Does Not Exist')
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+class AddPermsToUserView(View):
+    '''CBV for assigning permissions to user'''
+
+    @method_decorator(MustLogin)
+    def get(self, request, *args, **kwargs):
+        return redirect('dashboard:add_user_to_group')
+
+    @method_decorator(MustLogin)
+    def post(self, request, *args, **kwargs):
+        user_id = request.POST.get('user_id') or None
+        user = User.objects.filter(id=user_id).first()
+        user_permissions = request.POST.getlist('permissions')
+        if user:
+            user.user_permissions.clear()
+            for permission in user_permissions:
+                item = Permission.objects.filter(id=permission).first()
+                user.user_permissions.add(item)
+            user.save()
+            messages.success(request, 'User Permissions Updated Successfully')  # noqa
+        else:
+            messages.info(request, 'User Does Not Exist')
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
