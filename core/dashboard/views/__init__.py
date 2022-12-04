1475c5d3734f3257862713b1a2a33734fa3648d3
DevPrinceK <princesamuelpks@gmail.com>
2022-12-04 14:06:40 +0000

    Update __init__.py

--- core/dashboard/views/__init__.py
+++ core/dashboard/views/__init__.py
@@ -1,6 +1,4 @@
 from .messages import *
-from .views import *
-from .settings import *
 from .donations import *
 from .doctrines import *
 from .leaders import *
@@ -9,20 +7,6 @@ from .notifications import *
 from .users import *
 from .preachers import *
 from .categories import *
-
-from core.utils.util_functions import get_api_wallet_balance
-from accounts.models import User
-
-
-class DashboardView(View):
-    '''CBV for rendering the dashboard page'''
-
-    template = 'dashboard/dashboard.html'
-
-    def get(self, request):
-        context = {
-            'balance': get_api_wallet_balance(),
-            'users': User.objects.count(),
-            'staff_members': User.objects.filter(is_staff=True),
-        }
-        return render(request, self.template, context)
+from .groups import *
+from .user_profile import *
+from .dashboard import *
