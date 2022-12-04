2582d1fe1d3980e2ae7ff55d7ed8ce7a59192751
DevPrinceK <princesamuelpks@gmail.com>
2022-12-04 14:08:09 +0000

    Update notifications.py

--- core/dashboard/views/notifications.py
+++ core/dashboard/views/notifications.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.views import View
 
+from django.utils.decorators import method_decorator
+from core.utils.decorators import AdminOnly, MustLogin
+
 
 class NotificationsListView(View):
     '''CBV for listing notifications'''
     template = 'dashboard/notifications.html'
 
+    @method_decorator(MustLogin)
     def get(self, request, *args, **kwargs):
         context = {}
         return render(request, self.template, context)
