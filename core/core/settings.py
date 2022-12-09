bda07121ef1777b7357ee86222370a2ba8546a4f
DevPrinceK <princesamuelpks@gmail.com>
2022-12-09 22:37:51 +0000

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -152,8 +152,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles/'
 
-MEDIA_ROOT = BASE_DIR / '/assets/'
-MEDIA_URL = '/assets/'
+MEDIA_ROOT = BASE_DIR / '/media/'
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
