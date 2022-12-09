3e55e024e0e06a7036caa249412040f0d7ec1b7b
DevPrinceK <princesamuelpks@gmail.com>
2022-12-09 23:09:56 +0000

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -152,7 +152,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles/'
 
-MEDIA_ROOT = BASE_DIR / '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
