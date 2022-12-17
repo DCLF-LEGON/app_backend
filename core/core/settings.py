bc484416a40c52636a401f6b8bf3750decd130eb
DevPrinceK <princesamuelpks@gmail.com>
2022-12-17 02:33:31 +0000

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles/'
 
 MEDIA_ROOT = BASE_DIR / 'media/'
-MEDIA_URL = '/media/'
+MEDIA_URL = BASE_DIR / 'media/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
