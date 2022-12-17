fd653d4f166d410f813afacc7d8092758e4f3d1c
DevPrinceK <princesamuelpks@gmail.com>
2022-12-17 01:40:05 +0000

    Update urls.py

--- core/core/urls.py
+++ core/core/urls.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('api/', include('api.urls')),
+    path('docs/', include_docs_urls(title='DLCF APP API DOCS')),
     path('dashboard/', include('dashboard.urls')),
+    path('api-docs/', include('documentation.urls')),
 ]
 
 # rest_framework auth
