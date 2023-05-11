48dd0fbced18d3beccc1083f163eceebf8432826
DevPrinceK <princesamuelpks@gmail.com>
2023-05-11 10:13:05 +0000

    Update admin.py

--- core/dashboard/admin.py
+++ core/dashboard/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
 from accounts.models import OTP, User
+from dashboard.models import YoutubeVideo, MessageCategory, Preacher, Leader, Doctrine, Message
 
 admin.site.register(User)
 admin.site.register(OTP)
+admin.site.register(YoutubeVideo)
+admin.site.register(MessageCategory)
+admin.site.register(Preacher)
+admin.site.register(Leader)
+admin.site.register(Doctrine)
+admin.site.register(Message)
